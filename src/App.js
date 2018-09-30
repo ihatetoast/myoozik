382364a20737c8fd6129e7eabc12b98f4a4b55d4
Katy <ihatetoast@gmail.com>
2018-09-30 18:10:11 -0500

    added spotify deets

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import React, { Component } from 'react';
 import { Form, Icon, Input, Button } from 'antd';
 
 import './App.css';
-
+require('dotenv').config();
+const Spotify = require('node-spotify-api');
 const FormItem = Form.Item;
 
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+const spotify = new Spotify({
+  id: process.env.REACT_APP_SPOTIFY_ID,
+  secret: process.env.REACT_APP_SPOTIFY_SECRET
+});
+
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="App">
         <header>
           <h1>Myoozik</h1>
         </header>
-        <Form className="searchField">
+        <Form className="searchField" onSubmit={this.handleSubmit}>
           <FormItem>
-            <Input placeholder="enter artist/s" />
+            <Input
+              placeholder="enter artist/s"
+              value={this.state.query}
+              onChange={this.handleChange}
+            />
           </FormItem>
-          <Button type="primary">
+          <Button type="primary" htmlType="submit">
             <Icon type="search" theme="outlined" />
           </Button>
         </Form>
