52fc59fd42c70c067c4fc37e6e41916fcc87688b
Katy <ihatetoast@gmail.com>
2018-10-01 11:57:26 -0500

    added filler and some classNames to style a little btwn function work.

--- src/App.js
+++ src/App.js
@@ -35,25 +35,52 @@ export class App extends Component {
         <header>
           <h1>Myoozik</h1>
         </header>
-        <Form className="searchField" onSubmit={this.handleSubmit}>
-          <FormItem>
-            <Input
-              placeholder="enter artist/s"
-              value={this.state.query}
-              onChange={this.handleChange}
-            />
-          </FormItem>
-          <Button type="primary" htmlType="submit">
-            <Icon type="search" theme="outlined" />
-          </Button>
-        </Form>
-        <div className="searchResults">
-          <div className="artist-info">
-            <div className="name">Sweet</div>
-            <div className="info">meep mop moop</div>
-          </div>
-          <div className="albums">{'. . . '}</div>
-        </div>
+        <main>
+          <section className="intro-section">
+            <p>
+              Brief intro for what do to. Instructions. Meep mop moop. Beep bop
+              boop.
+            </p>
+          </section>
+          <section className="form-section">
+            <Form className="searchField" onSubmit={this.handleSubmit}>
+              <FormItem>
+                <Input
+                  placeholder="enter artist/s"
+                  value={this.state.query}
+                  onChange={this.handleChange}
+                />
+              </FormItem>
+              <Button type="primary" htmlType="submit">
+                <Icon type="search" theme="outlined" />
+              </Button>
+            </Form>
+          </section>
+          <section className="results-section">
+            <div className="searchResults">
+              <div className="artist-info">
+                <div className="name">Sweet</div>
+                <div className="info">
+                  <p>
+                    Lorem ipsum dolor amet tousled copper mug craft beer neutra
+                    iceland lo-fi keytar. Flannel trust fund fixie 3 wolf moon
+                    normcore salvia viral hella taxidermy godard. Pork belly
+                    readymade drinking vinegar, humblebrag leggings thundercats
+                    lomo hell of cred man bun echo park. Tbh crucifix green
+                    juice bitters polaroid, art party vice VHS iPhone butcher
+                    hoodie bespoke. Paleo pug hell of vape slow-carb, poutine
+                    bushwick retro twee 90's four loko unicorn hashtag kitsch.{' '}
+                  </p>
+                </div>
+              </div>
+              <div className="albums">
+                {
+                  '. . .be expecting lots of album cover divs here, not not yet, old mate . . . '
+                }
+              </div>
+            </div>
+          </section>
+        </main>
         <footer>
           <p>
             Katy Cassidy | ihatetoast | {new Date(Date.now()).getFullYear()}
